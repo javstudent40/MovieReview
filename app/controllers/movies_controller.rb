865e20153e9841270afe9c92bdb0690566904b1a
javstudent40 <titusgr30@yahoo.com>
2019-04-26 00:13:49 -0400

    added delete functionality to project

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -38,6 +38,13 @@ class MoviesController < ApplicationController
 
     end
 
+    def destroy
+      @movie = Movie.find(params[:id])
+      @movie.destroy
+
+      redirect_to movies_path
+    end
+
   
 
   private
