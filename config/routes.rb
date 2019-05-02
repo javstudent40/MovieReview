b1278852483b1b107e012618c8aac33839c73de2
javstudent40 <titusgr30@yahoo.com>
2019-05-01 21:19:46 -0400

    added Post model

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get 'movies/index'
   resources :movies
   root 'movies#index'
+  resources :posts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_27_070214) do
+ActiveRecord::Schema.define(version: 2019_05_02_011706) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.string "director"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
